7e0f3dd8b2e802b785aa48b0bfcd23115299ce03
Barkatul Mujauddin <barkatul.20@gmail.com>
2022-07-06 10:53:43 +0530

    Time: 9 ms (17.44%), Space: 44.4 MB (35.85%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,61 +1,51 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         
-        int[] ans=new int[nums1.length];
-        
-         if(nums1.length==0)
-        {
-             return ans;
-        }
-        if(nums2.length==0)
-        {
-            for(int i=0;i<nums1.length;i++)
-            {
-                nums1[i]=-1;
-            }
-            return ans;
-        }
-        
-        
         HashMap<Integer,Integer> map=new HashMap<>();
-        HashSet<Integer> set=new HashSet<>();
         
+        Stack<Integer> stack=new Stack<>();
+        
+        int[] ans=new int[nums1.length];
         boolean p=false;
         
-        for(int i=0;i<nums1.length;i++)
+        for(int i=nums2.length-1;i>=0;i--)
         {
-            set.add(nums1[i]);
-        }
-        
-        for(int i=0;i<nums2.length-1;i++)
-        {
-            if(set.contains(nums2[i]))
-            {
-            for(int j=i+1;j<nums2.length;j++)
-            {
-                p=false;
-                if(nums2[i]<nums2[j])
-                {
-                    p=true;
-                    map.put(nums2[i],nums2[j]);
-                    break;
-                }
-            }
-            if(!p)
+            if(stack.isEmpty())
             {
                 map.put(nums2[i],-1);
+                stack.push(nums2[i]);
             }
+            else
+            {
+                while(stack.peek() <= nums2[i])
+                {
+                    stack.pop();
+                    
+                    if(stack.isEmpty())
+                    {
+                        p=true;
+                        break;
+                    }
+                }
+                if(p)
+                {
+                    p=false;
+                    map.put(nums2[i],-1);
+                    stack.push(nums2[i]);
+                }
+                else
+                {
+                   map.put(nums2[i],stack.peek()); 
+                   stack.push(nums2[i]);
+                }
             }
-        }
-        if(set.contains(nums2[nums2.length-1]))
-        {
-            map.put(nums2[nums2.length-1],-1);
         }
         
         for(int i=0;i<nums1.length;i++)
         {
             ans[i]=map.get(nums1[i]);
         }
+        
         return ans;
     }
 }
