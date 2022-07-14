ed441dc2aeec591e4fc70f1b03ddeaca3743414d
Barkatul Mujauddin <barkatul.20@gmail.com>
2022-07-14 18:47:49 +0530

    Time: 24 ms (29.17%), Space: 51.9 MB (8.24%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,9 +1,20 @@
 class Solution {
+    
+    class Pair{
+        
+        int key;
+        int val;
+        
+        Pair(int key, int val){
+            this.key=key;
+            this.val=val;
+        }
+    }
+    
     public int[] topKFrequent(int[] nums, int k) {
         
         int[] ans=new int[k];
         
-        ArrayList<Integer> list=new ArrayList<>();
         HashMap<Integer,Integer> map=new HashMap<>();
         
         for(int i=0;i<nums.length;i++)
@@ -14,30 +25,50 @@ class Solution {
             }
             else
             {
-                list.add(nums[i]);
                 map.put(nums[i],1);
             }
         }
         
-        PriorityQueue<Integer> minH=new PriorityQueue<>();
+        PriorityQueue<Pair> minH=new PriorityQueue<>(new Comparator<Pair>() {
+            @Override
+            public int compare(Pair o1, Pair o2) {
+                if(o1.key == o2.key)
+                {
+                    if(o1.val > o2.val)
+                        return 1;
+
+                    return -1;
+                }
+                else
+                {
+                    if(o1.key > o2.key)
+                        return 1;
+
+                    return -1;
+                }
+            }
+        });
         
-        for(int i=0;i<list.size();i++)
+        for(int i=0;i<nums.length;i++)
         {
-            minH.add(map.get(list.get(i)));
-            
-            if(minH.size()>k)
+            if(map.containsKey(nums[i]))
             {
-                minH.poll();
+                
+                minH.add(new Pair(map.get(nums[i]),nums[i]));
+                
+                if(minH.size()>k)
+                {
+                    minH.poll();
+                }
+                
+                map.remove(nums[i]);
             }
         }
         
         int a=0;
-        for(int i=0;i<list.size();i++)
+        for(int i=0;i<k;i++)
         {
-            if(minH.contains(map.get(list.get(i))))
-            {
-                ans[a++]=list.get(i);
-            }
+            ans[a++]=minH.poll().val;
         }
         
         return ans;
