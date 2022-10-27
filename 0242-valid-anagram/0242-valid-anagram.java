23a71a069b0974009c4fc00f2d9e5ad456328b25
Barkatul Mujauddin <barkatul.20@gmail.com>
2022-10-27 12:52:41 +0530

    Time: 2 ms (99.25%), Space: 42.6 MB (89.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         
-        char c1[]=s.toCharArray();
+       /* char c1[]=s.toCharArray();
         char c2[]=t.toCharArray();
         
         Arrays.sort(c1);
@@ -10,6 +10,26 @@ class Solution {
         if(Arrays.equals(c1,c2))
             return true;
         
-        return false;
+        return false;*/
+        
+        int[] arr=new int[26];
+        
+        for(int i=0;i<s.length();i++)
+        {
+            arr[s.charAt(i) - 'a']++;
+        }
+        
+        for(int i=0;i<t.length();i++)
+        {
+            arr[t.charAt(i) - 'a']--;
+        }
+        
+        
+        for(int i=0;i<26;i++)
+        {
+            if(arr[i]!=0)
+                return false;
+        }
+        return true;
     }
 }
